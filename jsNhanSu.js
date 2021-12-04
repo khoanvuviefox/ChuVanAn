8d3e7e53af5df80abd674017f2d8967a6a6d3fe9
Vũ Khoan <93373792+khoanvuviefox@users.noreply.github.com>
2021-12-05 01:03:41 +0700

    Chỉnh sửa

--- jsNhanSu.js
+++ jsNhanSu.js
@@ -1,166 +1,139 @@
-class NhanSu {
-    constructor(id, fullname, avatar, dob, email, hometown, depart) {
-        this.id = id;
-        this.fullname = fullname;
-        this.avatar = avatar;
-        this.dob = dob;
-        this.email = email;
-        this.hometown = hometown;
-        this.department = depart;
+var selectedRow = null
+
+// Các chức năng của biểu mẫu
+function onFormSubmit() {
+    // kiểm tra tính hợp lệ
+    if (validate()) {
+        // lưu trữ dữ liệu người dùng
+        var formData = readFormData();
+        // kiểm tra hàng đang trống
+        if (selectedRow == null) {
+            // Chèn hồ sơ người dùng mới
+            insertNewRecord(formData);
+        } else {
+            // Cập nhật hồ sơ người dùng mới
+            updateRecord(formData);
+        }
+        // Đặt lại giá trị đầu vào
+        resetForm();
     }
 }
-
-let cacnhansu = [];
-
-function init() {
-    if (getLocalStorage(nhansu_data) == null) {
-        cacnhansu = [
-            new NhanSu(1, "Hoàng Khắc Thưởng", "", "01-01-2000", "hoangkhacthuong@cnald.com", "Thanh Hóa", "Hiệu trưởng, Bí thư Chi bộ"),
-            new NhanSu(2, "Nguyễn Ngọc Bảo", "", "02-01-2002", "nguyenngocbao@cvald.com", "Lâm Đồng", "Phó Hiệu trưởng"),
-        ];
-        setLocalStorage(nhansu_data, cacnhansu);
+// Nhập giá trị từ biểu mẫu
+function readFormData() {
+    var formData = {};
+    // Nhận giá trị từ đầu vào
+    formData["userName"] = document.getElementById("userName").value;
+    formData["DoB"] = document.getElementById("DoB").value;
+    formData["homeTown"] = document.getElementById("homeTown").value;
+    formData["position"] = document.getElementById("position").value;
+    formData["level"] = document.getElementById("level").value;
+    // trả về dữ liệu biểu mẫu
+    return formData;
+}
+// Chèn hồ sơ người dùng mới
+function insertNewRecord(data) {
+    var table = document.getElementById("stdlist").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.userName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.DoB;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.homeTown;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.position;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.level;
+    cell5 = newRow.insertCell(5);
+    cell5.innerHTML = `<a onClick="onEdit(this)">Chỉnh sửa</a>
+    <a onClick="onDelete(this)">Xóa bỏ</a>`;
+}
+// Đặt lại chức năng
+function resetForm() {
+    document.getElementById("userName").value = "";
+    document.getElementById("DoB").value = "";
+    document.getElementById("homeTown").value = "";
+    document.getElementById("position").value = "";
+    document.getElementById("level").value = "";
+    selectedRow = null;
+}
+// Chỉnh sửa chức năng
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("userName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("DoB").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("homeTown").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("position").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("level").value = selectedRow.cells[4].innerHTML;
+}
+// Cập nhật bản ghi
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.userName;
+    selectedRow.cells[1].innerHTML = formData.DoB;
+    selectedRow.cells[2].innerHTML = formData.homeTown;
+    selectedRow.cells[3].innerHTML = formData.position;
+    selectedRow.cells[4].innerHTML = formData.level;
+}
+// Xóa chức năng
+function onDelete(td) {
+    if (confirm('Bạn chắc chắn muốn xóa Nhân sự này?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("stdlist").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+// Kiểm tra xác thực người dùng
+function validate() {
+    isValid = true;
+    // xác thực Họ và tên
+    if (document.getElementById("userName").value == "") {
+        isValid = false;
+        document.getElementById("userNamevalidationError").classList.remove("hide");
     } else {
-        cacnhansu = getLocalStorage(nhansu_data);
+        isValid = true;
+        if (!document.getElementById("userNamevalidationError").classList.contains("hide")) {
+            document.getElementById("userNamevalidationError").classList.add("hide");
+        }
     }
-}
-
-function setLocalStorage(keyword, data) {
-    window.localStorage.setItem(keyword, JSON.stringify(data));
-}
-
-function getLocalStorage(keyword) {
-    return JSON.parse(window.localStorage.getItem(keyword));
-}
-
-function removeLocalStorage(keyword) {
-    window.localStorage.removeItem(keyword);
-}
-
-function showNhanSu() {
-    let tbNhanSu = document.getElementById("tbNhanSu");
-    tbNhanSu.innerHTML = '';
-    cacnhansu.forEach(function(nhansu, index) {
-        tbNhanSu.innerHTML += `
-             <tr id='tr_${nhansu.id}'>
-                 <td class = "text-center">${nhansu,id}</td>
-                 <td>${nhansu.fullname}</td>
-                 <td class = "text-center">
-                     <img class ="img-sm" src='${nhansu.avatar}'>
-                 </td>
-                 <td class="text-center">${nhansu.dob}</td>
-                 <td>${nhansu.email}</td>
-                 <td class="text-center">${nhansu.hometown}</td>
-                 <td class='text-center'>${nhansu.department}</td>
-                 <td>
-                     <button class="btn btn-success" onclick="edit(${nhansu.id})">Chỉnh sửa</button>
-                     <button class="btn btn-primary d-none" onclick="update(${nhansu.id})">Cập nhật</button>
-                     <button class="btn btn-warning d-none" onclick="cancel(${nhansu.id})">Hủy bỏ</button>
-                     <button class="btn btn-danger" onclick='removeNhanSu(${nhansu.id})'>Xóa bỏ</button>
-                 </td>
-             </tr>
-        `;
-    })
-}
-
-function save() {
-    let id = getNewId();
-    let fullname = document.getElementById("fullname").value;
-    let avatar = document.getElementById("avatar").value;
-    let dob = document.getElementById("dob").value;
-    let email = document.getElementById("email").value;
-    let salary = Number(document.getElementById("hometown").value);
-    let department = document.getElementById("department").value;
-    let nhansu = newNhanSu(id, fullname, avatar, dob, email, hometown, department);
-    cacnhansu.push(nhansu);
-    setLocalStorage(nhansu_data, cacnhansu);
-    showNhanSu();
-    clear();
-}
-
-function getNewId() {
-    return cacnhansu[cacnhansu.length - 1].id + 1;
-}
-
-function initDepartment() {
-    let department = document.getElementById("department");
-    departments.forEach(function(depart, index) {
-        department.innerHTML += `<option value='${depart}' ${depart == default_depart ? 'selected' : ''}>${depart}</option>`;
-    });
-}
-
-function clear() {
-    document.getElementById("fullname").value = '';
-    document.getElementById("avatar").value = "";
-    document.getElementById("dob").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("hometown").value = "";
-    document.getElementById("department").value = "";
-}
-
-function removeNhanSu(nhansuId) {
-    let position = findIndexNhanSu(nhansuId);
-    let confirmed = window.confirm("Bạn chắc chắn muốn xóa Nhân sự này?");
-    if (confirmed) {
-        cacnhansu.splice(position, 1);
-        setLocalStorage(nhansu_data, cacnhansu);
-        showNhanSu();
+    // xác thực Ngày sinh
+    if (document.getElementById("DoB").value == "") {
+        isValid = false;
+        document.getElementById("DoBvalidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("DoBvalidationError").classList.contains("hide")) {
+            document.getElementById("DoBvalidationError").classList.add("hide");
+        }
     }
+    // xác thực Quê quán
+    if (document.getElementById("homeTown").value == "") {
+        isValid = false;
+        document.getElementById("homeTownvalidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("homeTownvalidationError").classList.contains("hide")) {
+            document.getElementById("homeTownvalidationError").classList.add("hide");
+        }
+    }
+    // Xác thực CHức vụ
+    if (document.getElementById("position").value == "") {
+        isValid = false;
+        document.getElementById("positionvalidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("positionvalidationError").classList.contains("hide")) {
+            document.getElementById("positionvalidationError").classList.add("hide");
+        }
+    }
+    // xác thực Trình độ
+    if (document.getElementById("level").value == "") {
+        isValid = false;
+        document.getElementById("levelvalidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("levelvalidationError").classList.contains("hide")) {
+            document.getElementById("levelvalidationError").classList.add("hide");
+        }
+    }
+    return isValid;
 }
-
-function findIndexNhanSu(nhansuId) {
-    return cacnhansu.findIndex(function(nhansu, index) {
-        return nhansu.id == nhansuId;
-    });
-}
-
-function edit(nhansuId) {
-    let tr = document.getElementById(`tr_${nhansuId}`);
-    tr.children[7].children[0].classList.add('d-none');
-    tr.children[7].children[1].classList.remove('d-none');
-    tr.children[7].children[2].classList.remove('d-none');
-
-    let index = findIndexNhanSu(nhansuId);
-    let nhansu = cacnhansu[index];
-    tr.children[1].innerHTML = `<input id="fn_${nhansuId}" class='input-full' type='text' value='${nhansu.fullname}'>`;
-    tr.children[2].innerHTML = `<input id="avatar_${nhansuId}" class='input-full' type='text' value='${nhansu.avatar}'>`;
-    tr.children[3].innerHTML = `<input id="dob_${nhansuId}" class='input-full' type='date' value='${nhansu.dob}'>`;
-    tr.children[4].innerHTML = `<input id="email_${nhansuId}" class='input-full' type='email' value='${nhansu.email}'>`;
-    tr.children[5].innerHTML = `<input id="hometown_${nhansuId}" class='input-full' type='text' value='${nhansu.hometown}'>`;
-    tr.children[6].innerHTML = `<input id="depart_${nhansuId}" class='input-full' type='text' value='${nhansu.department}'>`;
-}
-
-function update(nhansuId) {
-    let index = findIndexNhanSu(nhansuId);
-    cacnhansu[index].fullname = document.getElementById(`fn_${nhansuId}`).value;
-    cacnhansu[index].avatar = document.getElementById(`avatar_${nhansuId}`).value;
-    cacnhansu[index].dob = document.getElementById(`dob_${nhansuId}`).value;
-    cacnhansu[index].email = document.getElementById(`email_${nhansuId}`).value;
-    cacnhansu[index].hometown = document.getElementById(`hometown_${nhansuId}`).value;
-    cacnhansu[index].department = document.getElementById(`depart_${nhansuId}`).value;
-    setLocalStorage(nhansu_data, cacnhansu);
-    showNhanSu();
-}
-
-function cancel(nhansuId) {
-    let tr = document.getElementById(`tr_${nhansuId}`);
-    tr.children[7].children[0].classList.remove('d-none');
-    tr.children[7].children[1].classList.add('d-none');
-    tr.children[7].children[2].classList.add('d-none')
-    let index = findIndexNhanSu(nhansuId);
-    let nhansu = cacnhansu[index];
-    tr.children[1].innerHTML = nhansu.fullname;
-    tr.children[2].innerHTML = `<img class='img-sm' src='${nhansu.avatar}'>`;
-    tr.children[3].innerHTML = nhansu.dob;
-    tr.children[4].innerHTML = nhansu.email;
-    tr.children[5].innerHTML = nhansu.salary;
-    tr.children[6].innerHTML = nhansu.department;
-}
-
-function ready() {
-    init();
-    initDepartment();
-    clear();
-    showNhanSu();
-}
-
-ready();
